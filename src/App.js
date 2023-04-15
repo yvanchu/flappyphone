a1a34cdf20dab2a071f866b95a827c5bbece56fd
Yvan Chu <60163603+yvanchu@users.noreply.github.com>
2023-04-15 13:37:35 -0500

    added permission seeking for phone and created folder for flappyScreen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  const [isRunning, setIsRunning] = useState(false);
+
+  const handlePermissions = () => {
+    // Request permission for iOS 13+ devices
+    if (
+      DeviceMotionEvent &&
+      typeof DeviceMotionEvent.requestPermission === "function"
+    ) {
+      DeviceMotionEvent.requestPermission();
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <button onClick={handlePermissions}>Start</button>
+        </p>
+      </header>
+    </div>
+  );
 }
 
 export default App;
